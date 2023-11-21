3cf1f4e782f9bb51bd31cb92bf66632de973e39d
The Khai <c0a221537d@edu.teu.ac.jp>
2023-11-21 17:43:31 +0900

    最終版 :q 最終版

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,6 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    y = 0
     kk_img = [kk_img, pg.transform.rotozoom(kk_img,1,1),pg.transform.rotozoom(kk_img,2,1),pg.transform.rotozoom(kk_img,3,1),pg.transform.rotozoom(kk_img,4,1),
               pg.transform.rotozoom(kk_img,5,1),pg.transform.rotozoom(kk_img,6,1),pg.transform.rotozoom(kk_img,7,1),
               pg.transform.rotozoom(kk_img,8,1),pg.transform.rotozoom(kk_img,9,1),pg.transform.rotozoom(kk_img,10,1),
@@ -20,14 +19,15 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(pg.transform.flip(bg_img,True,False), [1600-x,0])
-        screen.blit(kk_img[tmr%21],[300,200])
+        screen.blit(bg_img,[3200-x,0])
+        screen.blit(pg.transform.flip(bg_img,True,False),[4800-x,0])
+        screen.blit(kk_img[tmr%len(kk_img)],[300,200])
         pg.display.update()
         tmr += 1
-        y += 1
-        clock.tick(50)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
